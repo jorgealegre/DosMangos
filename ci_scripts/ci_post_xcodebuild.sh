bd4f21992707af59fcbd16bc6c712996fe48c153
Jorge Alegre <jorge@alegre.dev>
2026-01-01 14:39:48 -0400

    Show last 5 commits in release notes

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,6 +4,6 @@
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
-  echo "Last 3 commits:" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-  git fetch --deepen 3 && git log -3 --pretty=format:"%s" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+  echo "Last 5 commits:" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+  git fetch --deepen 5 && git log -5 --pretty=format:"%s" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
