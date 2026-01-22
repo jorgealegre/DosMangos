46fa760a9d008683016e7915114b2caf55eb7b44
Jorge Alegre <jorge@alegre.dev>
2026-01-22 00:18:12 -0300

    Update tests

--- DosMangosTests/DosMangosTests.swift
+++ DosMangosTests/DosMangosTests.swift
@@ -29,7 +29,7 @@ extension BaseTestSuite {
             await store.send(\.view.task)
 
             await store.send(\.transactionsList.view.onAppear)
-            #expect(store.state.transactionsList.rows.isEmpty)
+            #expect(store.state.transactionsList.data.rows.isEmpty)
 
             var transaction = Transaction.Draft()
             await store.send(\.view.newTransactionButtonTapped) {
@@ -51,36 +51,81 @@ extension BaseTestSuite {
                 $0.destination = nil
             }
 
-            try await store.state.transactionsList.$rows.load()
-            assertInlineSnapshot(of: store.state.transactionsList.rows, as: .customDump) {
+            try await store.state.transactionsList.$data.load()
+            assertInlineSnapshot(of: store.state.transactionsList.data, as: .customDump) {
                 """
-                [
-                  [0]: TransactionsListRow(
-                    transaction: Transaction(
-                      id: UUID(00000000-0000-0000-0000-000000000000),
-                      description: "Rent",
-                      valueMinorUnits: 12300,
-                      currencyCode: "USD",
-                      convertedValueMinorUnits: 12300,
-                      convertedCurrencyCode: "USD",
-                      type: .expense,
-                      createdAtUTC: Date(2009-02-13T23:31:30.000Z),
-                      localYear: 2009,
-                      localMonth: 2,
-                      localDay: 13,
-                      recurringTransactionID: nil
-                    ),
-                    category: nil,
-                    tags: [],
-                    location: TransactionLocation(
-                      transactionID: UUID(00000000-0000-0000-0000-000000000000),
-                      latitude: -34.6037,
-                      longitude: -58.3816,
-                      city: "Córdoba",
-                      countryCode: "AR"
+                TransactionsList.DataRequest.Value(
+                  rows: [
+                    [0]: TransactionsListRow(
+                      transaction: Transaction(
+                        id: UUID(00000000-0000-0000-0000-000000000000),
+                        description: "Rent",
+                        valueMinorUnits: 12300,
+                        currencyCode: "USD",
+                        convertedValueMinorUnits: 12300,
+                        convertedCurrencyCode: "USD",
+                        type: .expense,
+                        createdAtUTC: Date(2009-02-13T23:31:30.000Z),
+                        localYear: 2009,
+                        localMonth: 2,
+                        localDay: 13,
+                        recurringTransactionID: nil
+                      ),
+                      category: nil,
+                      tags: [],
+                      location: TransactionLocation(
+                        transactionID: UUID(00000000-0000-0000-0000-000000000000),
+                        latitude: -34.6037,
+                        longitude: -58.3816,
+                        city: "Córdoba",
+                        countryCode: "AR"
+                      )
                     )
-                  )
-                ]
+                  ],
+                  dueRows: [],
+                  rowsByDay: [
+                    13: [
+                      [0]: TransactionsListRow(
+                        transaction: Transaction(
+                          id: UUID(00000000-0000-0000-0000-000000000000),
+                          description: "Rent",
+                          valueMinorUnits: 12300,
+                          currencyCode: "USD",
+                          convertedValueMinorUnits: 12300,
+                          convertedCurrencyCode: "USD",
+                          type: .expense,
+                          createdAtUTC: Date(2009-02-13T23:31:30.000Z),
+                          localYear: 2009,
+                          localMonth: 2,
+                          localDay: 13,
+                          recurringTransactionID: nil
+                        ),
+                        category: nil,
+                        tags: [],
+                        location: TransactionLocation(
+                          transactionID: UUID(00000000-0000-0000-0000-000000000000),
+                          latitude: -34.6037,
+                          longitude: -58.3816,
+                          city: "Córdoba",
+                          countryCode: "AR"
+                        )
+                      )
+                    ]
+                  ],
+                  balanceByDay: [
+                    13: Money(
+                      value: -12300,
+                      currencyCode: "USD"
+                    )
+                  ],
+                  days: [
+                    [0]: 13
+                  ],
+                  incomeTotal: 0,
+                  expenseTotal: 12300,
+                  currencyCode: "USD",
+                  topCategories: []
+                )
                 """
             }
 
